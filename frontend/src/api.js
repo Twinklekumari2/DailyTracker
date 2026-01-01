9480357e9321bd6d940bc38c8be8003d3f4965fc
Twinklekumari2 <twinklekumari246@gmail.com>
2026-01-01 23:26:19 +0530

    vercel.json added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:"http://localhost:3000/",
+//   baseURL:"http://localhost:3000/",
+  baseURL:"https://dailytracker-2hj3.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json"
